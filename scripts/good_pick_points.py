4fcff85714f5ac653af22b3def76fb3d37a81eef
HRI25 Group 3 <hri25-group3@ur5e.robots.constructor.university>
2025-10-29 12:40:27 +0100

    29.10.2025
    code to select good trajectories
    chnage sin the jupyter notebook to visualize the poses and trajectories
    next step we create a code to select the trajectories from the current pose and update pose
    Andres (-_-)

--- scripts/good_pick_points.py
+++ scripts/good_pick_points.py
@@ -7,14 +7,11 @@ import moveit_msgs.msg
 with open("/home/hri25-group3/ros/src/safety_perception/joint_snapshot1.pkl", "rb") as f:
         data = pickle.load(f)
 
-rospy.init_node('notebook')
-state_pub = rospy.Publisher("notebook_state", moveit_msgs.msg.DisplayRobotState)
-
 IN_PKL  = "joint_snapshot1.pkl"
-OUT_PKL = "good_pairs.pkl"
+OUT_PKL = "good_pairs_fast.pkl"
 GROUP   = "arm"
 NAMED   = "ready"    # or None
-VEL     = 0.5
+VEL     = 0.6
 
 def to_js(s):
     js = JointState()
@@ -23,10 +20,9 @@ def to_js(s):
     return js
 
 def main():
-    moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node("label_pairs_short", anonymous=True)
 
-    state_pub = rospy.Publisher("notebook_state", DisplayRobotState, queue_size=1, latch=True)
+    state_pub = rospy.Publisher("notebook_state", moveit_msgs.msg.DisplayRobotState, queue_size=1, latch=True)
 
 
     with open(IN_PKL, "rb") as f: data = pickle.load(f)
@@ -37,59 +33,75 @@ def main():
     #for ready
     if NAMED:
         move.set_named_target(NAMED)
-        move.go(wait=True)
-        move.stop()
+        while not move.go(wait=True):
+            rospy.logerr("Robot did not arrive to READY position")
+            if not rospy.is_shutdown():
+                return 
+            rospy.sleep(3.0)
+
+
 
     # append to existing file if present
     try:
         with open(OUT_PKL, "rb") as f: good = pickle.load(f)
-        if not isinstance(good, list): good = []
+        if not isinstance(good,dict):
+            raise
     except Exception:
-        good = []
+        rospy.logwarn("could not load dictionary start from scratch")
+        good = dict()
 
     print("Enter=save, x=ignore, q=quit\n")
 
     for i in range(len(data)-1):
-        for j in range(i+1, len(data)):
-            start = data[i]
-            end = data[j]
-            js1, js2 = to_js(start), to_js(end)
-
-            # --- start -> end ---
+        start = data[i]
+        js1=to_js(start)
+        try:
             move.set_joint_value_target(js1)
-            move.go(wait=True)
-            move.stop()
+            if not move.go(wait=True):
+                raise
+        except Exception:
+            rospy.logerr(f"Cannot reach pose {i} skipping starting point")
+            continue
 
-            move.set_joint_value_target(js2)
-            ok = move.go(wait=True)
-            move.stop()
-
-            if not ok:# planning problem
-                print(f"skip {i}->{j} (plan failed)")
+        for j in range(i+1, len(data)):
+            if (i,j) in good:
+                rospy.loginfo(f"skipping known trajectory from {i} to {j}")
                 continue
+            end = data[j]
+            js2 = to_js(end)
+            rospy.loginfo(f"{js2}")
+            try:
+                move.set_joint_value_target(js2)
+            except Exception:
+                rospy.logwarn("js2 failed")
+
+            ok,plan,planning_time,error_code = move.plan()
+       
+            if not ok:# planning problem
+                print(f"skip {i}->{j} (second position not reachable, failed)")
+                continue
+            move.execute(plan, wait=True)
             #waiting for keyboard input
             ans = input(f"[{i}->{j}] Enter=save, x=ignore, q=quit > ").strip().lower()
             
             if ans == "q": 
-                raise KeyboardInterrupt
+                return
             if ans != "x":
-                good.append({"dir":"i_to_j","i":i,"j":j,"start":start,"end":end})
+                good[(i,j)]= plan
                 with open(OUT_PKL,"wb") as f: pickle.dump(good,f,pickle.HIGHEST_PROTOCOL)
-                print(f"saved ({i}->{j})")
+                print(f"saved ({i}->{j})")  
 
             # --- end -> start ---
             move.set_joint_value_target(js1)
-            ok = move.go(wait=True)
-            move.stop()
-            if not ok:
-                print(f"return {j}->{i} failed")
-                continue
-            ans = input(f"[{j}->{i}] Enter=save, x=ignore, q=quit > ").strip().lower()
-            if ans == "q": raise KeyboardInterrupt
-            if ans != "x":
-                good.append({"dir":"j_to_i","i":j,"j":i,"start":end,"end":start})
-                with open(OUT_PKL,"wb") as f: pickle.dump(good,f,pickle.HIGHEST_PROTOCOL)
-                print(f"saved ({j}->{i})")
+            while not move.go(wait=True):
+                rospy.logerr("Robot did not arrive to READY position")
+                if not rospy.is_shutdown():
+                    return 
+                rospy.sleep(3.0)
 
     print(f"done. total saved: {len(good)}")
-    moveit_commander.roscpp_shutdown()
+
+
+if __name__ =="__main__":
+    main()
+
