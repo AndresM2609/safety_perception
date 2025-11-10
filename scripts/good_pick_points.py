11a68d3edec4ebed2b1a571807f4282d54e297b3
HRI25 Group 3 <hri25-group3@ur5e.robots.constructor.university>
2025-11-10 14:35:18 +0100

    10.11.2025 from Anderson we finish the backwards movement,robot can now move from different trajectory, the speed is ready, the last part will be record actual demo(points and trajctories), program is ready

--- scripts/good_pick_points.py
+++ scripts/good_pick_points.py
@@ -8,10 +8,12 @@ with open("/home/hri25-group3/ros/src/safety_perception/joint_snapshot1.pkl", "r
         data = pickle.load(f)
 
 IN_PKL  = "joint_snapshot1.pkl"
-OUT_PKL = "good_pairs2_fast.pkl"
+OUT_PKL = "test_pairs_fast.pkl"
 GROUP   = "arm"
 NAMED   = "ready"    # or None
-VEL     = 0.6
+VEL_SCALING_FACTOR = 0.8  # fraction of limits #for slow 0.6
+MAX_CARTESIAN_SPEED = 0.7#m/s #for slow 0.3
+#for slow motions
 
 def to_js(s):
     js = JointState()
@@ -28,7 +30,8 @@ def main():
     with open(IN_PKL, "rb") as f: data = pickle.load(f)
 
     move = moveit_commander.MoveGroupCommander(GROUP)
-    move.set_max_velocity_scaling_factor(VEL)
+    move.set_max_velocity_scaling_factor(VEL_SCALING_FACTOR)
+    move.limit_max_cartesian_link_speed(MAX_CARTESIAN_SPEED,'arm_tool0')
     
     #for ready
     if NAMED:
@@ -92,7 +95,28 @@ def main():
                 print(f"saved ({i}->{j})")  
 
             # --- end -> start ---
+            move.set_start_state_to_current_state()
             move.set_joint_value_target(js1)
+            ok_rev, plan_rev, _, _ = move.plan()
+
+            if ok_rev:
+                # Execute reverse and save it
+                move.execute(plan_rev, wait=True)
+                good[(j, i)] = plan_rev
+                with open(OUT_PKL, "wb") as f:
+                    pickle.dump(good, f, pickle.HIGHEST_PROTOCOL)
+                print(f"saved reverse ({j}->{i})")
+            else:
+                rospy.logwarn(f"reverse plan {j}->{i} failed; returning with simple go()")
+                move.set_joint_value_target(js1)
+                move.go(wait=True)
+
+        else:
+            # Not saving forward: just go back without saving reverse
+            move.set_joint_value_target(js1)
+            move.go(wait=True)
+
+
             while not move.go(wait=True):
                 rospy.logerr("Robot did not arrive to READY position")
                 if not rospy.is_shutdown():
